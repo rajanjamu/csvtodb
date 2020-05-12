f1c71ae824cba16c696f50cbe4de1bf3035aff31
rajanjamu <rajanjamu@gmail.com>
2020-05-12 19:27:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello Boy!')
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port)
+})
